aef19c0eec113f40ffe998a5cb459dea0371b652
trick12138 <2663495373@qq.com>
2021-11-13 16:22:05 +0800

    main

--- outclass work/shutdown your computer.c
+++ outclass work/shutdown your computer.c
@@ -4,7 +4,7 @@
 int main()
 {
     int a;
-    printf("请输入一个数字来执行操作:\n1.立即关机(1分钟后) 2.立即重启 3.等待1.5小时后关机\n");
+    printf("请输入一个数字来执行操作:\n1.立即关机(1分钟后) 2.立即重启 3.等待1.5小时后关机 4.取消关机\n");
     scanf("%d",&a);
     switch (a)
     {
@@ -13,7 +13,9 @@ int main()
     case 2:system("shutdown -r");
         break;
     case 3:system("shutdown -s -t 1800");
-    default:
+        break;
+    case 4:system("shutdown -a");
+    default: printf("请输入一个正确的数字");
         break;
     }
 }
