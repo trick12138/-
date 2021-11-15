13b81cfe72a140ce8f60ceb99b2b4ac4814ce567
trick12138 <trick12138@github.com>
2021-11-16 01:16:56 +0800

    main

--- inclass work/capture 3/easy caculation of circle.c
+++ inclass work/capture 3/easy caculation of circle.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #define Pi 3.1415926
-
 int main()
 {
     float r,h;    //声明两个变量
@@ -11,6 +10,6 @@ int main()
     printf("圆的周长为%0.2f\n",2*r*Pi);    //输出
     printf("圆的面积为%f\n",r*r*Pi);
     printf("圆球表面积为%f\n",4*Pi*r);
-    printf("圆球体积为%f\n",4/3*r*r*r*Pi);
+    printf("圆球体积为%f\n",(float)4/(float)3*r*r*r*Pi);//易错点这里如果不用类型转化而直接4/3会直接得到1,因为int和int相乘,得到int,所以要进行类型转化,转一个就行了,因为浮点和整形的运算会自动都转化为浮点
     printf("圆柱体积为%f\n",Pi*r*r*h);
 }
