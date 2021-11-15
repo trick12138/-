3945a8a19f8e44647cb04404c15071441d7e6114
trick12138 <trick12138@github.com>
2021-11-15 13:17:49 +0800

    main

--- inclass work/capture 4/page 109.c
+++ inclass work/capture 4/page 109.c
@@ -1,39 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int a,c,d;
-    float b;
+    int a,b,c,e,f;
     scanf("%d",&a);
     while (a != 0)
     {
-        b = (float)a/10;
-        a = a/10;
-        b = b - a;
-        c = (int)(b * 10);
-        d = ++d;
-        printf("%d\n",a);
-        printf("%f\n",b);
-        printf("%d\n",c);
+        b = a / 10;
+        b = a - b * 10;
+        a = a / 10;
+        e = b + c * 10;
+        c = e;
+        printf("%d\n",b);
+        f = ++f;
     }
-    printf("位数=%d",d);
-    
+    printf("%d\n",e);
+    printf("位数为%d",f);
 }
-/*
-98745
-9874
-0.500000
-5
-987
-0.400024
-4
-98
-0.699997
-6
-9
-0.800000
-8
-0
-0.900000
-9
-位数=5
-*/
