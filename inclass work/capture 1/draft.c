7d456ce6da21b7abaa00d28f9dde4880ab72e72c
trick12138 <trick12138@github.com>
2021-11-16 22:28:03 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    printf("%d",-5 & 6);
+    int a,b;
+    float x,y;
+    char c1,c2;
+    scanf("a=%db=%d",&a,&b);
+    scanf("%f%e",&x,&y);
+    scanf("%c%c",&c1,&c2);
+    printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c",a,b,x,y,c1,c2);
+    return 0;
 }
