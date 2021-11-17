551560f5780325bdb5ee87e43fe9ed6c49704665
trick12138 <2663495373@qq.com>
2021-11-17 20:57:36 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char a;
-	scanf("%c",&a);
-	if (a>='A'&&a<='Z')
-	printf("为大写字母");
-	else if(a>='0'&&a<='9')
-	printf("为数字字符");
-	else if(a>='a'&&a<='z')
-	printf("为小写字母");
-	else if(a==' ')
-	printf("为空格字符");
-	else
-	printf("为其他字符"); 
-	return 0;
+    int a;
+    printf("%d\n",a = 65432/pow(10,4));
+    printf("%d\n",a=pow(10,1));
 }
