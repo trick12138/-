c25d97a4318449d4e322bbcfc87b26faf6e86782
trick12138 <2663495373@qq.com>
2021-11-17 14:32:43 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    
+    int i,j;
+    for (i=1;i<=4;i++)
+    {
+        for (j=1;j<=5;j++)
+        {
+            printf("%d\t",i*j);
+            if (j%5==0)
+            printf("\n");
+        }
+    }
+    return 0;
 }
