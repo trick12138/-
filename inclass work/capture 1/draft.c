3ccae3dd780aafe1a475b8f33e7cfea732bbceb2
trick12138 <2663495373@qq.com>
2021-11-17 19:42:01 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,19 +1,20 @@
-#include<stdio.h> 
-int main()
-{
- int sth;
- scanf("%c",&sth);
- if(sth>='0'&&sth<='9')
-  {
-  	printf("sth是一个数字");
-  }
- if(sth>='A'&&sth<='Z')
- {
- 	printf("sth是一个大写字母");
- }
- if(sth>='a'&&sth<='z')
- {
- 	printf("sth是一个小写字母");
- }
- return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char a;
+	scanf("%c",&a);
+	if (a>='A'&&a<='Z')
+	printf("为大写字母");
+	else if(a>='0'&&a<='9')
+	printf("为数字字符");
+	else if(a>='a'&&a<='z')
+	printf("为小写字母");
+	else if(a==' ')
+	printf("为空格字符");
+	else
+	printf("为其他字符"); 
+	return 0;
 }
