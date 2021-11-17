78e981bfc8308caa51ba18eddee8563a5886c833
trick12138 <2663495373@qq.com>
2021-11-17 15:43:12 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int i,j;
-    for (i=1;i<=4;i++)
+    //声明变量
+    int a,b;
+    a=1;
+    //从2开始寻找质数
+    while (a<=100000)//此处为最大的值
     {
-        for (j=1;j<=5;j++)
-        {
-            printf("%d\t",i*j);
-            if (j%5==0)
-            printf("\n");
-        }
+        a=a+1;
+        b=a;
+       while (b!=1)
+       {
+           b=b-1;
+           if (a%b==0)
+           break;
+           else
+             if (b==2)
+             printf(" %d",a);
+       }
     }
-    return 0;
 }
