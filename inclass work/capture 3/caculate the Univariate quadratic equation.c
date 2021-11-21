1d6568e3197dc19992afecd31f59d20693425365
trick12138 <2663495373@qq.com>
2021-11-21 23:03:13 +0800

    main

--- inclass work/capture 3/caculate the Univariate quadratic equation.c
+++ inclass work/capture 3/caculate the Univariate quadratic equation.c
@@ -7,14 +7,14 @@
 
 int main()
 {
-    float a,b,c;
+    float a,b,c,x1,x2;
     printf("请输入abc的值,用空格隔开");
     scanf("%f %f %f",&a,&b,&c);
     if ((b*b-4*a*c)>=0)
     {
-        a=(-b+sqrt(b*b-4*a*c))/2*a;
-        b=(-b-sqrt(b*b-4*a*c))/2*a;
-        printf("x1=%f,x2=%f",a,b);
+        x1=(-b+sqrt(b*b-4*a*c))/2*a;
+        x2=(-b-sqrt(b*b-4*a*c))/2*a;
+        printf("x1=%f,x2=%f",x1,x2);
     }
     if ((b*b-4*a*c<0))
     {
