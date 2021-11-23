5f44a75e1460251cc3801b09a0d3a2e70f4f6fac
trick12138 <2663495373@qq.com>
2021-11-23 22:49:21 +0800

    main

--- inclass work/capture 1/draft.c
+++ inclass work/capture 1/draft.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&(a/10));
-    printf("%d",a);
+    //使用指针遍历一维数组
+    int *pa;
+    int b[10]={1,2,3,4,5,6,7,8,9,10};
+    pa = b;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",*(pa + i));
+    }
+    printf("\n");
+    //使用指针遍历二维数组
+    int c[5][5];
+    pa = c;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; i++)
+        {
+            printf("%d ",*((*pa + i)+j));
+        }
+        
+    }
+    
 }
