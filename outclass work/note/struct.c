7ea90ca6eb88f1040ffc3a11097b1cc1244e0b0f
trick12138 <2663495373@qq.com>
2021-11-26 18:07:04 +0800

    main

---
+++ outclass work/note/struct.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    //结构体的长度
+    struct ceo
+    {
+        int a;
+        char b;
+        int c;
+    };
+    printf("长度=%d",sizeof(struct ceo));
+    //结构体的自引用
+    
+}
