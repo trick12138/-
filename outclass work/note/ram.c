315e6dde9f27a42dcf409cf56661fd93ffd1f4bf
trick12138 <2663495373@qq.com>
2021-11-29 22:25:35 +0800

    main

--- outclass work/note/ram.c
+++ outclass work/note/ram.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 int main()
 {   //动态开辟内存空间
     //malloc 直接分配
@@ -30,5 +32,17 @@ int main()
     //free 用于释放内存
     free(pa);
     free(pb);
+    //内存操作函数
+    //memccpy
+    //定义一个结构体
+    typedef struct ram
+    {
+        int a;
+        char b;
+    }ram1;
+    ram1 node1 = {1,'A'},node2;
+    memcpy(&node2,&node1,sizeof(ram1));
+    char *pd = (char*)&node2;
+    printf("第一个为%d,第二个为%c",*(pd + 0),*(pd + 4));
     return 0;
 }
