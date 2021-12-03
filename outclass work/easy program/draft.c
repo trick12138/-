71968e30107158d344d8f8a06ae136a762a3a2c6
trick12138 <2663495373@qq.com>
2021-12-03 09:16:18 +0800

    main

--- outclass work/easy program/draft.c
+++ outclass work/easy program/draft.c
@@ -1,31 +1,6 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int i = 0;
-    char a[100] = "1234";
-    char b[100] = "12";
-    char d[100];
-    while(i < 5)
-    {
-        if ( *(b+i) > '0'&& *(b+i) < '9')
-        {
-            i++;
-            continue;
-        }
-        else
-        {
-            *(b+i) = '0';
-            i++;
-        }
-        
-    }
-/*     while (i < 4)
-    {
-        *( d + i) = *(a + i) + *(b + i) - '0';
-        i++;
-    } */
-    for ( i = 0; i < 4; i++)
-    {
-        printf("%c",*( b + i));
-    }
+    printf("%d",strlen(''));
 }
