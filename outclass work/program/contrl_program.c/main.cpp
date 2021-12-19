2af7076c9a5541edb68f3e9df0ca85e94cb7a044
trick12138 <2663495373@qq.com>
2021-12-19 15:32:57 +0800

    main

--- outclass work/program/contrl_program.c/main.cpp
+++ outclass work/program/contrl_program.c/main.cpp
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<graphics.h>
-
+void screen()
+{
+    initgraph(640,480);
+    setbkcolor(WHITE);
+    setcaption("成绩管理系统");
+}
 
 int main()
 {
-    
+    screen();
+    getchar();
     return 0;
 }
